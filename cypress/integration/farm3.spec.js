94ac048afcdc9308823c3367b8d040ffe1088608
baominh <leminhbao2709@gmail.com>
2021-10-03 23:20:05 +0700

    new customer

---
+++ cypress/integration/farm3.spec.js
@@ -0,0 +1,48 @@
+/// <reference types="Cypress" />
+import { loginPA } from "../support/PageAction/LoginPA"
+import { homePagePA } from "../support/PageAction/HomePagePA"
+import { foodStorePA } from "../support/PageAction/FoodStorePA"
+import { slackNotification } from "../support/Ultil/SlackNotification";
+
+beforeEach(function () {
+    cy.fixture('pkdUser_03').then((petkingdom) => {
+        Cypress.config('slack_channel', '#a_farmer_03_b')
+        // slackNotification.sendMsgToSlackAndTelegram('Start game for email: ' + petkingdom.email)
+        slackNotification.cypressInitialEnvironmentlog(petkingdom.email, petkingdom.primaryPet, petkingdom.fuckPet, Cypress.config('slack_channel'))
+    })
+});
+
+afterEach(function () {
+    cy.fixture('pkdUser_03').then((petkingdom) => {
+        slackNotification.sendMessagetoSlackWithTag('End game for email: ' + petkingdom.email)
+        slackNotification.sendMessagetoTelegram('End game for email: ' + petkingdom.email)
+    })
+});
+
+describe('pkdUser_01: Feed one 1 pet to 100 stamina', () => {
+    it('Feed 1 pet to 100 stamina', {
+        retries: {
+            runMode: 30,
+            openMode: 1,
+        },
+    }, () => {
+
+
+        cy.fixture('pkdUser_03').then((petkingdom) => {
+            loginPA.visitPetKingDom();
+            loginPA.loginPKDScholarMode(petkingdom.email, petkingdom.password);
+            slackNotification.sendMsgToSlackAndTelegram('Start game for email: ' + petkingdom.email + ' - select world: ' + petkingdom.world)
+            homePagePA.selectWorld(petkingdom.world)
+            homePagePA.selectFoodStore();
+            foodStorePA.feedThePetTo100(petkingdom.primaryPet, petkingdom.fuckPet, petkingdom.dificulty, petkingdom.email)
+        })
+    })
+
+    it('MMMMM', () => {
+        cy.log('END')
+
+    })
+
+
+})
+
